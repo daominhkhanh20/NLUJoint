627df9cdf231df103968cd0c27238b5960a14570
daominhkhanh20 <khanhc1k36@gmail.com>
2023-01-04 22:46:03 +0700

    update relabel

--- nlu_transformer/inference/infer_joint.py
+++ nlu_transformer/inference/infer_joint.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 import os 
 from nlu_transformer.utils.io import * 
@@ -62,14 +63,33 @@ class InferenceJoint:
             return list_intents, list_slots
     
     def evalute_test(self, path_folder: str):
+        if not os.path.exists(os.path.join(path_folder, 'log_failed')):
+            os.makedirs(os.path.join(path_folder, 'log_failed'), exist_ok=True)
+
         for folder_test in os.listdir(path_folder):
+            if 'test' not in folder_test:
+                continue
             current_path = os.path.join(path_folder, folder_test)
             list_sentence = read_file(os.path.join(current_path, 'seq.in'))
             ground_truth_intents, ground_truth_slots = load_label(os.path.join(current_path, 'ground_truth.csv'))
             pred_intents, pred_slots = self.inference(list_sentence)
             cnt = 0
+            tmp_sentence, tmp_truth, tmp_pred = [], [], []
             for idx in range(len(pred_intents)):
                 if ground_truth_intents[idx].strip() == pred_intents[idx].strip() and ground_truth_slots[idx].strip() == pred_slots[idx].strip():
                     cnt += 1
-            print(f"Accuracy for {folder_test}: {cnt/ len(pred_intents)}")
+                else:
+                    tmp_sentence.append(list_sentence[idx])
+                    if ground_truth_intents[idx].strip() != pred_intents[idx].strip() and ground_truth_slots[idx].strip() != pred_slots[idx].strip():
+                        tmp_truth.append(f"{ground_truth_intents[idx].strip()}|{ground_truth_slots[idx].strip()}")
+                        tmp_pred.append(f"{pred_intents[idx].strip()}|{pred_slots[idx].strip()}")
+                    elif ground_truth_intents[idx].strip() != pred_intents[idx].strip():
+                        tmp_truth.append(ground_truth_intents[idx].strip())
+                        tmp_pred.append(pred_intents[idx].strip())
+                    elif ground_truth_slots[idx].strip() != pred_slots[idx].strip():
+                        tmp_truth.append(ground_truth_slots[idx].strip())
+                        tmp_pred.append(pred_slots[idx].strip())
+                df = pd.DataFrame({"sentence": tmp_sentence, "truth": tmp_truth, "pred": tmp_pred})
+                df.to_csv(f"{os.path.join(path_folder, 'log_failed')}/{folder_test}.csv")
+            print(f"Accuracy for {folder_test} = {cnt}/{len(pred_intents)} ~ {cnt/ len(pred_intents)}")
         
